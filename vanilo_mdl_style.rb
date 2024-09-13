96b99fd5d55a9023c2eb4c120ef5bce77fc9c4ab
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2024-09-13 10:50:17 +0300

    Added markdown linter style + CI action

---
+++ vanilo_mdl_style.rb
@@ -0,0 +1,28 @@
+rule 'MD002'
+rule 'MD003', :style => :atx
+rule 'MD004', :style => :dash
+rule 'MD005'
+rule 'MD006'
+rule 'MD007'
+rule 'MD010'
+rule 'MD011'
+rule 'MD012'
+rule 'MD018'
+rule 'MD019'
+rule 'MD022'
+rule 'MD023', :allow_different_nesting => true
+rule 'MD025'
+rule 'MD026', :punctuation => '.,;:'
+rule 'MD027'
+rule 'MD028'
+rule 'MD029', :style => :ordered
+rule 'MD030'
+rule 'MD031'
+rule 'MD032'
+rule 'MD035', :style => '---'
+rule 'MD037'
+rule 'MD039'
+rule 'MD040'
+rule 'MD041'
+rule 'MD046', :style => :fenced
+rule 'MD047'
