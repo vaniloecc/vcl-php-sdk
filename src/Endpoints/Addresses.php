23a0adbb01bb6bd3e00546c9211c50925bf67832
Ervin Molnar <58359015+molnarerwin@users.noreply.github.com>
2025-02-21 08:16:02 +0200

    Code cleanup in Addresses trait

--- src/Endpoints/Addresses.php
+++ src/Endpoints/Addresses.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace VaniloCloud\Endpoints;
 
-use Illuminate\Support\Collection;
 use VaniloCloud\Models\Address;
 use VaniloCloud\WriteModels\AddressUpdate;
-use VaniloCloud\WriteModels\ProductCreate;
 
 trait Addresses
 {
@@ -28,53 +26,6 @@ trait Addresses
 
         return new Address($this->transpose($response->json(), Address::class));
     }
-
-    /**
-     * Get all products with optional includes.
-     *
-     * @param array $includes Optional data to include.
-     * @return Collection A collection of Product objects.
-     */
-    /*public function products(array $includes = []): Collection
-    {
-        $url = "/products";
-
-        if (!empty($includes)) {
-            $url .= '?include=' . implode(',', $includes);
-        }
-
-        $response = $this->get($url);
-
-        $result = collect();
-        if ($response->successful()) {
-            foreach ($response->json('data') as $item) {
-                $product = new Product($this->transpose($item, Product::class));
-
-                $result->put($product->id, $product);
-            }
-        }
-
-        return $result;
-    }*/
-
-    /**
-     * Create a new product.
-     *
-     * @param ProductCreate $product The product data to create.
-     * @return string|null The ID of the created product, or null on failure.
-     */
-    /*public function createProduct(ProductCreate $product): ?string
-    {
-        $response = $this->post('/products', $product->toArray());
-
-        if (201 !== $response->status()) {
-            return null;
-        }
-
-        $urlParts = explode('/', $response->header('Location'));
-
-        return end($urlParts);
-    }*/
 
     /**
      * Update an existing address by id.
